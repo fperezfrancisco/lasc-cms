33779cd11c2c91b7d2db50a2038359478079e0d5
Francisco Perez <fperezfrancisco4@gmail.com>
2025-08-18 09:08:43 -0700

    removed old strapi setup for fresh start

--- src/api/news-media/routes/news-media.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * news-media router
- */
-
-const { createCoreRouter } = require('@strapi/strapi').factories;
-
-module.exports = createCoreRouter('api::news-media.news-media');
--- src/api/news-media/services/news-media.js
+++
@@ -1,9 +0,0 @@
-'use strict';
-
-/**
- * news-media service
- */
-
-const { createCoreService } = require('@strapi/strapi').factories;
-
-module.exports = createCoreService('api::news-media.news-media');
